048223a9ae705e57c9372b2b75145a06404810eb
Robin Lecullier <lecullier.robin@gmail.com>
2023-09-22 11:55:02 +0200

    admin panel secure

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import PanelAdmin from './components/Admin/PanelAdmin';
 import Login from './components/Login';
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+import { collection, doc, getDoc, getDocs, query, where } from 'firebase/firestore'; // Utilisez getDoc au lieu de getDocs
+import { db } from './firebase';
 
 
 function App() {
+
   return (
     <div className="App">
       <Router>
-          <Routes>
-            <Route exact path='/admin' Component={PanelAdmin} />
-            <Route exact path='/login' Component={Login} />
-          </Routes>
-        </Router>
+        <Routes>
+          <Route exact path='/admin' Component={PanelAdmin} />
+          <Route exact path='/login' Component={Login} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Admin/PanelAdmin.js
+++ src/components/Admin/PanelAdmin.js
@@ -1,13 +1,62 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Container, Modal, Offcanvas } from 'react-bootstrap';
 import FormUsers from './FormUsers';
 import ListUsers from './ListUsers';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../firebase';
+import { collection, doc, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { Redirect } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom/dist';
 
 const PanelAdmin = () => {
   const [showListeModal, setShowListeModal] = useState(false);
   const [showOffCanvas, setShowOffCanvas] = useState(false);
   const [userListKey, setUserListKey] = useState(0);
-  const [userToEdit, setUserToEdit] = useState(null); // Initialisez userToEdit à null
+  const [userToEdit, setUserToEdit] = useState(null);
+  const [user, setUser] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (authUser) => {
+      if (authUser) {
+        setUser(authUser);
+
+        try {
+          const q = query(collection(db, 'users'), where('uid', '==', authUser.uid), where('role', '==', 'admin'));
+          const querySnapshot = await getDocs(q);
+
+          if (!querySnapshot.empty) {
+            setIsAdmin(true);
+          } else {
+            setIsAdmin(false);
+          }
+        } catch (error) {
+          console.error('Erreur lors de la récupération des données de l\'utilisateur :', error);
+        }
+      } else {
+        setUser(null);
+        setIsAdmin(false);
+      }
+
+      setIsLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAdmin) {
+    // Si l'utilisateur n'est pas administrateur, redirigez-le vers une autre page
+    navigate('/login')
+  }
 
   const handleShowListeModal = () => {
     setShowListeModal(true);
